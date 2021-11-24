5667edbdd8297749b8d3f07eba6ced409ee26e2b
ngockhue1620 <ngockhuentca2k@gmail.com>
2021-11-24 18:17:13 +0700

    login logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @user.presence || redirect_to(root_url)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(sign_in_param)
+    if @user.save
+      flash[:success] = t "mess.sign_in_success"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def sign_in_param
+    params.require(:user).permit(:name, :email, :user_name, :password,
+                                 :password_confirmation)
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get '/login', to: "home#new"
+  post '/login', to: 'home#create'
+  delete "/logout", to: "home#destroy"
+
+  resources :home
 end
